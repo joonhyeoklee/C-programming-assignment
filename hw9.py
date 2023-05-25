807f1489e9b1be5cc4d732acaf86e7f8238e56ef
joonhyeoklee <127420227+joonhyeoklee@users.noreply.github.com>
2023-05-25 21:30:10 +0900

    hw9.py

--- hw9.py
+++ hw9.py
@@ -6,7 +6,7 @@ class Rectangle :
     self.__y2 = y2
   
   def show(self):
-    print(f'좌측 상단 꼭지점이 ({self.__x1},{self.__y1})이고 우측 상단 꼭지점이 ({self.__x2},{self.__y2})인 사각형입니다.')
+    print(f'좌측 상단 꼭지점이 ({self.__x1},{self.__y1})이고 우측 하단 꼭지점이 ({self.__x2},{self.__y2})인 사각형입니다.')
   
   def getWidth(self):
     return (self.__x2 - self.__x1)
